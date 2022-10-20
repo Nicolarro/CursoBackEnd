689022381ad2476ae5cb8efdeacbc27c36a7f833
Nicolas Larrosa <86521241+Nicolarro@users.noreply.github.com>
2022-10-19 23:45:09 -0300

    Final Version Desafio "Manejo de Archivos"

---
+++ desafio2Contenedor.js
@@ -0,0 +1,115 @@
+const fs = require ("fs")
+
+class Contenedor {
+    constructor (archivo) {
+        this.archivo = archivo;
+        this.productos = []
+    }
+
+  async save(producto) {
+    try{
+        if(fs.existsSync(this.archivo)) {
+
+           let productos =  await fs.promises.readFile(this.archivo)
+           if( productos.length>0){
+            let lastId = productos[productos.length-1].id + 1
+            let newProduct={
+                id: lastId ,
+               ...producto
+              }
+
+              productos.push(newProduct);
+              await fs.promises.writeFile(this.archivo,JSON.stringify(productos,null,2))
+              return lastId;
+        } 
+        else
+        {
+            let lastId=1
+            let newProduct={
+              id: lastId,
+             ...producto
+        }
+
+            productos.push(newProduct);
+            await fs.promises.writeFile(this.archivo,JSON.stringify(productos,null,2))
+            return lastId;
+       }
+       
+    }else{
+       let newProduct={
+        id:1,
+        title: producto.title,
+        price: producto.price,
+        thumbnail: producto.thumbnail
+            }
+        await fs.promises.writeFile(this.archivo,JSON.stringify([newProduct],null,2));
+        return 1;
+        }
+        } catch (error) {
+        console.log(error)
+    }
+}
+async getAll () { 
+    try {
+        if(fs.existsSync(this.archivo)){
+        let info= await fs.promises.readFile(this.archivo, 'utf8');
+        let result= JSON.parse(info);
+        return result;
+        }else{
+          return "No se encontro el archivo"
+        } 
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+    }
+
+
+    async deleteAll() {
+        try {
+
+            let nuevoArray = [];
+            console.log(`Borrando datos...`);
+            await this.writeFile(this.archivo, nuevoArray);
+        } catch (error) {
+            console.log(
+                `Ocurrio un error eliminando los datos: ${error.message}`
+            );
+        }
+    }
+
+    async deleteById(id) {
+        try {
+            if (this.exists(this.archivo)) {
+                const data = await this.readFile(this.archivo);
+                if (data.some(item => item.id === id)) {
+                    const data = await this.readFile(this.archivo);
+                    const datos = data.filter(item => item.id !== id);
+                    this.writeFile(this.archivo, datos);
+                } else {
+                    throw new Error(
+                        `No se encontro el producto con el id ${id}`
+                    );
+                }
+            }
+        } catch (error) {
+            console.log(
+                `Ocurrio un error eliminando el producto con el id solicitado: ${error.message}`
+            );
+        }
+    }
+}
+    
+
+
+
+
+let contenedor = new Contenedor ("productos.txt")
+
+
+module.exports = Contenedor
+
+
+
+
---
+++ desafio2main.js
@@ -0,0 +1,35 @@
+/* Importado de Contenedor */
+
+const Contenedor = require("./desafio2Contenedor")
+
+let contenedor= new Contenedor("productos.txt")
+
+let producto1 ={
+    id:1,
+    title:"Silla Gamer",
+    price:60000,
+    thumbnail:"https://d3ugyf2ht6aenh.cloudfront.net/stores/001/720/526/products/diseno-sin-titulo-5121-bd311cff4d158548d516372654110148-1024-1024.jpg"
+
+}
+let producto2 ={
+    id:2,
+    title:"Joystick PS5",
+    price:25000,
+    thumbnail:"https://images.fravega.com/f1000/a23c2e9cbe114eca833fc5f7288457fc.jpg"
+
+}
+let producto3 ={
+    id: 3,
+    title:"Auriculares PS5",
+    price:4200,
+    thumbnail:"https://arsonyb2c.vtexassets.com/arquivos/ids/348062/PS5_WHS_Pshot_A.jpg?v=637363806123470000"
+
+}
+metodos=async()=>{
+   console.log( await contenedor.save(producto1))
+   console.log( await contenedor.save(producto2))
+  console.log( await contenedor.save(producto3))
+ console.log(await contenedor.getAll())
+console.log(await contenedor.deleteById()) 
+}
+ metodos()
