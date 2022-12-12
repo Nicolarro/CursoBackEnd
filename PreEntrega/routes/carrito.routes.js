3d54024a46e1fdc1ee27bf1d0a914ddfc6805e17
Nicolas Larrosa <nicolas.larrosa@accenture.com>
2022-12-12 16:16:36 -0300

    Modifying Methods

--- PreEntrega/routes/carrito.routes.js
+++ PreEntrega/routes/carrito.routes.js
@@ -1,17 +1,46 @@
-import {Router} from "express";
-import { ProductManager } from "../products.js";
+import { Router } from "express";
+import { CartManager } from "../cartManager.js";
+import { ProductManager } from "../productManager.js";
 
-const router =Router();
+const router = Router();
 
-export const carrito = new ProductManager("./carrito.json");
+export const carritos= new CartManager("./carrito.json");
+export const productos = new ProductManager("./products.json");
 
-router.post("/", async (req,res) => {
+router.post("/", async (req, res) => {
     const products = []
-    const id = await carrito.
-    const carrito = {id, ...products}
-    const nuevoCarrito = carrito.
-    res.send({status:true, carrito: carrito})
+    const idCarrito = await carritos.getCartID();
+    const carrito = { idCarrito, ...products }
+    const nuevoCarrito = carritos.push(carrito)
+    res.send({ success: true, nuevoCarrito: nuevoCarrito })
+});
+
+router.get("/:cid", async (req, res) => {
+    const idCarrito = parseInt(req.params.cid);
+    const findCarrito = await carritos.getCartById(idCarrito);
+    if (!findCarrito) {
+        res.send({ success: false, message: "Cart Not Found" })
+    }
+    else {
+        res.send({ success: true, carrito: findCarrito })
+    }
+
 })
+
+router.post("/:id/productss/:pid", async (req, res) => {
+    const idCarrito = parseInt(req.params.id);
+    const idProducto = req.body.idProducto;
+    const carrito = await carritos.getCartById(idCarrito);
+    const carritoJson = carrito[0];
+    const producto = await productos.getById(idProducto);
+    const productoJson = producto[0];
+    await carritoJson.productos.push(productoJson);
+    await carritos.modify(idCarrito, carritoJson);
+    res.status(201).send(carritoJson);
+});
+
+
+
 
 
 
--- PreEntrega/routes/products.routes.js
+++ PreEntrega/routes/products.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { ProductManager } from "../products.js";
+import { ProductManager } from "../productManager.js";
 
 export const instancia = new ProductManager("./products.json");
 
