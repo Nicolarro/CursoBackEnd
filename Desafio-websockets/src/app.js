9c7c58c49d0b703e7ad8a62310e888003f5ff357
Nicolas Larrosa <nicolas.larrosa@accenture.com>
2022-12-22 18:15:44 -0300

    Adding Handlebars

--- Desafio-websockets/src/app.js
+++ Desafio-websockets/src/app.js
@@ -1,22 +1,64 @@
 import express from "express";
 import productRouter from "./routes/products.routes.js";
 import carritoRouter from "./routes/carrito.routes.js";
+import handlebars from "express-handlebars";
+import __dirname from "./dirname.js";
+import { Server as HttpServer } from "http";
+import { Server as IOServer } from "socket.io";
+
+const port = 8080;
 
 const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = 8080;
+
 
 app.engine(
-  "handlebar",
-  handlebar.engine({ extname: ".hbs", defaultLayout: "main.hbs" })
+    "handlebar",
+    handlebars.engine({ extname: ".hbs", defaultLayout: "main.hbs" })
 );
 
-app.set();
+app.set("views engine", "hbs");
+app.set("views", `${__dirname}/views`);
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.set("io", io);
 
 app.use("/api/products", productRouter);
 app.use("/api/carts", carritoRouter);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const server = httpServer.listen(PORT, () =>
+    console.log(`Server running on port ${server.address().port}`)
+);
+
+server.on("error", (error) => {
+    console.log(error);
+});
+
+io.on("connection", async (socket) => {
+    console.log(`New client connected, id: ${socket.id}`);
+
+    io.sockets.emit("hello", "HOLA!");
+
+    const products = await productManager.getProducts();
+
+    io.sockets.emit("products", products);
+
+    socket.on("addProduct", async (product) => {
+        try {
+            await productManager.saveProduct(product);
+
+            // volvemos a enviar todos los productos
+            io.sockets.emit("products", await productManager.getProducts());
+        } catch (error) {
+            console.log(error);
+        }
+    });
+});
--- PreEntrega/app.js
+++ PreEntrega/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import productRouter from "./routes/products.routes.js"
-import carritoRouter from "./routes/carrito.routes.js"
+import cartRouter from "./routes/carrito.routes.js"
 
 const app = express()
 
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 const port = 8080
 
 app.use('/api/products', productRouter)
-app.use('/api/carts', carritoRouter)
+app.use('/api/carts', cartRouter)
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
