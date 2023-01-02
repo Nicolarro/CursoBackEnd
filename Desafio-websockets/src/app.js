1b58f0877d52a3ca268f9ad0590385a535e894fe
Nicolas Larrosa <86521241+Nicolarro@users.noreply.github.com>
2023-01-02 19:52:02 -0300

    Modifyin App.js

--- Desafio-websockets/src/app.js
+++ Desafio-websockets/src/app.js
@@ -15,11 +15,9 @@ const io = new IOServer(httpServer);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.engine(
-    "handlebar",
-    handlebars.engine({ extname: ".hbs", defaultLayout: "main.hbs" })
+  "handlebar",
+  handlebars.engine({ extname: ".hbs", defaultLayout: "main.hbs" })
 );
 
 app.set("views engine", "hbs");
@@ -35,30 +33,30 @@ app.use("/api/products", productRouter);
 app.use("/api/carts", carritoRouter);
 
 const server = httpServer.listen(PORT, () =>
-    console.log(`Server running on port ${server.address().port}`)
+  console.log(`Server running on port ${server.address().port}`)
 );
 
 server.on("error", (error) => {
-    console.log(error);
+  console.log(error);
 });
 
 io.on("connection", async (socket) => {
-    console.log(`New client connected, id: ${socket.id}`);
+  console.log(`New client connected, id: ${socket.id}`);
 
-    io.sockets.emit("hello", "HOLA!");
+  io.sockets.emit("hello", "HOLA!");
 
-    const products = await productManager.getProducts();
+  const products = await productManager.getProducts();
 
-    io.sockets.emit("products", products);
+  io.sockets.emit("products", products);
 
-    socket.on("addProduct", async (product) => {
-        try {
-            await productManager.saveProduct(product);
+  socket.on("addProduct", async (product) => {
+    try {
+      await productManager.saveProduct(product);
 
-            // volvemos a enviar todos los productos
-            io.sockets.emit("products", await productManager.getProducts());
-        } catch (error) {
-            console.log(error);
-        }
-    });
+      // volvemos a enviar todos los productos
+      io.sockets.emit("products", await productManager.getProducts());
+    } catch (error) {
+      console.log(error);
+    }
+  });
 });
